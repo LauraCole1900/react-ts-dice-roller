d3423facf88d1640800cc204ca0ffd9f5d33f151
Laura Cole <lauracole1900@comcast.net>
2021-05-30 21:10:40 -0600

    renamed setDice to setType

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,11 @@ function App() {
   // Need to lift state from dice to here?
   // Math.random here? or in dice components?
   // Will need to throw results into an array in state for display
-  
+
   let diceType: number;
   let roll: number = 0;
 
-  const setDice = (e: any) => {
+  const setType = (e: any) => {
     diceType = e.target.value;
   }
 
@@ -38,15 +38,15 @@ function App() {
         <Row className="center">
           <Col sm={12}>
             <h3>Die type:</h3>
-            <Button className="button" value={2} onClick={setDice}>d2</Button>
-            <Button className="button" value={3} onClick={setDice}>d3</Button>
-            <Button className="button" value={4} onClick={setDice}>d4</Button>
-            <Button className="button" value={6} onClick={setDice}>d6</Button>
-            <Button className="button" value={8} onClick={setDice}>d8</Button>
-            <Button className="button" value={10} onClick={setDice}>d10</Button>
-            <Button className="button" value={12} onClick={setDice}>d12</Button>
-            <Button className="button" value={20} onClick={setDice}>d20</Button>
-            <Button className="button" value={1} onClick={setDice}>d&d set</Button>
+            <Button className="button" value={2} onClick={setType}>d2</Button>
+            <Button className="button" value={3} onClick={setType}>d3</Button>
+            <Button className="button" value={4} onClick={setType}>d4</Button>
+            <Button className="button" value={6} onClick={setType}>d6</Button>
+            <Button className="button" value={8} onClick={setType}>d8</Button>
+            <Button className="button" value={10} onClick={setType}>d10</Button>
+            <Button className="button" value={12} onClick={setType}>d12</Button>
+            <Button className="button" value={20} onClick={setType}>d20</Button>
+            <Button className="button" value={1} onClick={setType}>d&d set</Button>
           </Col>
         </Row>
 
