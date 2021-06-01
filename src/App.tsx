797627bc0ddd53924726d4853836f6ee37704849
Laura Cole <lauracole1900@comcast.net>
2021-05-31 20:34:08 -0600

    results of single-die roll render to page; 'roll' button conditionally renders

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,22 @@ function App() {
   // Math.random here? or in dice components?
   // Will need to throw results into an array in state for display
 
-  let diceType: number = 0;
-  let roll: number = 0;
-  // const [dice, setDice] = useState<number | undefined>()
+  const [diceType, setDiceType] = useState<number>(0);
+  const [roll, setRoll] = useState<number | undefined>();
 
   const setType = (e: any) => {
-    diceType = JSON.parse(e.target.value);
-    console.log({ diceType });
-    console.log(typeof diceType);
+    const dType = JSON.parse(e.target.value);
+    console.log({ dType });
+    console.log(typeof dType);
+    setDiceType(dType);
   }
 
   const diceRoll = () => {
     if (diceType !== 1) {
-      roll = Math.floor(Math.random() * diceType) + 1;
-      console.log(roll);
-      console.log(typeof roll);
-      return roll
+      const dRoll: number = Math.floor(Math.random() * diceType) + 1;
+      console.log(dRoll);
+      console.log(typeof dRoll);
+      setRoll(dRoll);
     } else if (diceType === 1) {
 
     } else if (diceType === 0) {
@@ -34,9 +34,9 @@ function App() {
     }
   }
 
-  useEffect(() => {
-    diceRoll();
-  }, [diceType, roll])
+  // useEffect(() => {
+  //   diceRoll();
+  // }, [diceType, roll])
 
   return (
     <>
@@ -48,29 +48,29 @@ function App() {
         <Row className="center">
           <Col sm={12}>
             <h3>Die type:</h3>
-            <Button className="button" value={2} onClick={setType} active={diceType === 2}>d2</Button>
-            <Button className="button" value={3} onClick={setType} active={diceType === 3}>d3</Button>
-            <Button className="button" value={4} onClick={setType} active={diceType === 4}>d4</Button>
-            <Button className="button" value={6} onClick={setType} active={diceType === 6}>d6</Button>
-            <Button className="button" value={8} onClick={setType} active={diceType === 8}>d8</Button>
-            <Button className="button" value={10} onClick={setType} active={diceType === 10}>d10</Button>
-            <Button className="button" value={12} onClick={setType} active={diceType === 12}>d12</Button>
-            <Button className="button" value={20} onClick={setType} active={diceType === 20}>d20</Button>
-            <Button className="button" value={1} onClick={setType} active={diceType === 1}>d&d set</Button>
+            <Button className="button" value={2} onClick={setType} target={diceType === 2}>d2</Button>
+            <Button className="button" value={3} onClick={setType} target={diceType === 3}>d3</Button>
+            <Button className="button" value={4} onClick={setType} target={diceType === 4}>d4</Button>
+            <Button className="button" value={6} onClick={setType} target={diceType === 6}>d6</Button>
+            <Button className="button" value={8} onClick={setType} target={diceType === 8}>d8</Button>
+            <Button className="button" value={10} onClick={setType} target={diceType === 10}>d10</Button>
+            <Button className="button" value={12} onClick={setType} target={diceType === 12}>d12</Button>
+            <Button className="button" value={20} onClick={setType} target={diceType === 20}>d20</Button>
+            <Button className="button" value={1} onClick={setType} target={diceType === 1}>d&d set</Button>
           </Col>
         </Row>
 
-        {/* {diceType !== 0 && */}
+        {diceType !== 0 &&
           <Row className="center">
             <Button className="rollBtn" onClick={diceRoll}>Roll!</Button>
           </Row>
-        {/* } */}
+        }
 
-        {/* {roll !== 0 && */}
+        {roll !== undefined &&
           <Row className="center">
             <h3>{roll}</h3>
           </Row>
-          {/* } */}
+        }
 
       </Container>
     </>
